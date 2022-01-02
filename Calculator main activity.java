d50d9704e60a7aebeee3af40515e2e56b494194a
FuryPhoenix4 <96972381+FuryPhoenix4@users.noreply.github.com>
2022-01-02 13:16:52 +0530

    Create Calculator main activity.java

---
+++ Calculator main activity.java
@@ -0,0 +1,165 @@
+package com.example.cal_hari;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btn_mult,btn_div,btn_equal,btn_per,btn_add,btn_sub,btn_clear;
+    TextView inputText,outputText;
+    private String input,output,newOutput;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputText=findViewById(R.id.input_text);
+        outputText=findViewById(R.id.output_text);
+        btn0=findViewById(R.id.btn0);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+        btn5=findViewById(R.id.btn5);
+        btn6=findViewById(R.id.btn6);
+        btn7=findViewById(R.id.btn7);
+        btn8=findViewById(R.id.btn8);
+        btn9=findViewById(R.id.btn9);
+        btn_mult=findViewById(R.id.btn_mult);
+        btn_add=findViewById(R.id.btn_add);
+        btn_sub=findViewById(R.id.btn_sub);
+        btn_clear=findViewById(R.id.btn_clear);
+        btn_div=findViewById(R.id.btn_division);
+        btn_equal=findViewById(R.id.btn_equal);
+        btn_per=findViewById(R.id.btn_per);
+
+
+    }
+
+    public void onButtonClicked(View view) {
+
+        Button button = (Button) view;
+        String data = button.getText().toString();
+        switch (data) {
+            case "C":
+                input = null;
+                output=null;
+                newOutput=null;
+                outputText.setText("");
+                break;
+
+            case "^":
+                solve();
+                input += "^";
+                break;
+            case "*":
+                solve();
+                input += "*";
+                break;
+
+            case "=":
+                solve();
+                break;
+
+            case "%":
+                input += "%";
+                double d = Double.parseDouble(inputText.getText().toString()) / 100;
+                outputText.setText(String.valueOf(d));
+                break;
+
+            default:
+                if (input == null) {
+                    input = "";
+                }
+                if (data.equals("+") || data.equals("/") || data.equals("-")) {
+                    solve();
+                }
+                input += data;
+        }
+        inputText.setText(input);
+    }
+
+    private void solve() {
+        if (input.split("\\+").length == 2) {
+            String numbers[] = input.split("\\+");
+            try {
+                double d = Double.parseDouble(numbers[0]) + Double.parseDouble(numbers[1]);
+                output = Double.toString(d);
+                newOutput = cutDecimal(output);
+                outputText.setText(newOutput);
+                input = d +"";
+            }catch (Exception e) {
+                outputText.setText(e.getMessage().toString());
+            }
+        }
+        if (input.split("\\*").length == 2) {
+            String numbers[] = input.split("\\*");
+            try {
+                double d = Double.parseDouble(numbers[0]) * Double.parseDouble(numbers[1]);
+                output = Double.toString(d);
+                newOutput = cutDecimal(output);
+                outputText.setText(newOutput);
+                input = d +"";
+            }catch (Exception e){
+                outputText.setText(e.getMessage().toString());
+            }
+        }
+        if (input.split("\\/").length == 2) {
+            String numbers[] = input.split("\\/");
+            try {
+                double d = Double.parseDouble(numbers[0]) / Double.parseDouble(numbers[1]);
+                output = Double.toString(d);
+                newOutput = cutDecimal(output);
+                outputText.setText(newOutput);
+                input = d +"";
+            }catch (Exception e){
+                outputText.setText(e.getMessage().toString());
+            }
+        }
+        if (input.split("\\^").length == 2) {
+            String numbers[] = input.split("\\^");
+            try {
+                double d = Math.pow(Double.parseDouble(numbers[0]), Double.parseDouble(numbers[1]));
+                output = Double.toString(d);
+                newOutput = cutDecimal(output);
+                outputText.setText(newOutput);
+                input = d +"";
+            }catch (Exception e){
+                outputText.setText(e.getMessage().toString());
+            }
+        }
+        if (input.split("\\-").length == 2) {
+            String numbers[] = input.split("\\-");
+            try {
+                if (Double.parseDouble(numbers[0]) < Double.parseDouble(numbers[1])){
+                    double d = Double.parseDouble(numbers[1]) - Double.parseDouble(numbers[0]);
+                    output = Double.toString(d);
+                    newOutput = cutDecimal(output);
+                    outputText.setText("-" + newOutput);
+                    input = d +"";
+                }
+                else {
+                    double d = Double.parseDouble(numbers[0]) - Double.parseDouble(numbers[1]);
+                    output = Double.toString(d);
+                    newOutput = cutDecimal(output);
+                    outputText.setText(newOutput);
+                    input = d + "";
+                }
+            }catch (Exception e){
+                outputText.setText(e.getMessage().toString());
+            }
+        }
+    }
+    private String cutDecimal(String number){
+        String n [] = number.split("\\.");
+        if (n.length >1){
+            if (n[1].equals("0")){
+                number = n[0];
+            }
+        }
+        return number;
+    }
+}
